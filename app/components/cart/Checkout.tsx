04d5ff02199152242a08f674194c87b7cee3ab15
linkoten <catto.francois@hotmail.fr>
2025-04-06 19:18:27 +0200

    re4444

--- app/components/cart/Checkout.tsx
+++ app/components/cart/Checkout.tsx
@@ -10,7 +10,7 @@ export default function Checkout(user: any) {
   const [loading, setLoading] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState("France");
+  const [selectedCountry, setSelectedCountry] = useState("");
 
   useEffect(() => {
     const checkStockAvailability = () => {
