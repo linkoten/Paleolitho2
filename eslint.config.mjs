dc2ad8b87f92beb5e45335af9398167550e90084
linkoten <catto.francois@hotmail.fr>
2025-04-06 19:08:26 +0200

    encore une fois

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+
+  }
 ];
 
-// Ignorer les erreurs any
-eslintConfig[0].rules["@typescript-eslint/no-explicit-any"] = "off";
 
 export default eslintConfig;
